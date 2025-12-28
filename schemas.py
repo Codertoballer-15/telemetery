90f7ef9bc21af2fffb42f356f2a562e901cec483
Codertoballer-15 <jeetadatiya65@gmail.com>
2025-12-28 11:28:45 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,12 +1,8 @@
 from pydantic import BaseModel
-from typing import List
 
 class Attempt(BaseModel):
-    time: float
     code: str
-    error: str
     success: bool
 
 class AnalysisRequest(BaseModel):
-    task_goal: str
-    attempts: List[Attempt]
+    attempts: list[Attempt]
